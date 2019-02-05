125cca8b4f1bdf0dc078c08a9ba4d87cd9d7ba8a
XO <kirhhoff@gmail.com>
2019-02-05 10:12:33 +0200

    feature/1 #Lesson1 #1 code refactoring, formatting, fixes after review

--- src/com/javaguru/shoppinglist/AbstractValidator.java
+++ src/com/javaguru/shoppinglist/AbstractValidator.java
@@ -1,6 +1,6 @@
 package com.javaguru.shoppinglist;
 
-public abstract class AbstractValidator implements Validator{
+public abstract class AbstractValidator implements Validator {
 
     boolean assertNotNull(String str) {
         return str != null && !str.isEmpty();
